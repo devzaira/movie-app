fc6892511cada4f6195b5f901284b79aedb59367
Zaira Cruz <devzaira@gmail.com>
2025-05-07 02:56:53 +0800

    feat: add header layout with search

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import { useState } from 'react'
+import Search from "./components/Search.jsx";
 
 const App = () => {
+    const [searchTerm, setSearchTerm] = useState('');
+
     return (
-        <div>
-            <h1 className="text-3xl font-bold underline">
-                Hello world!
-            </h1>
-        </div>
+        <main>
+            <div className="pattern" />
+            <div className="wrapper">
+                <header>
+                    <img src="/public/hero.png" alt="Hero Banner"/>
+                    <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
+                </header>
+            </div>
+            <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        </main>
     )
 }
 export default App
